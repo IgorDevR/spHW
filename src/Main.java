4fb1fb19a5c60304ed029a7abf4a622d0d602511
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-09-23 01:29:16 +0300

    2.6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        hw1(nums);
+        hw2(nums);
+
+        String words = "qwe qwe ewq ewq rty asd zxc vbn jkl ";
+        hw3(words);
+        hw4(words);
+
     }
+
+    public static void hw1(List<Integer> values) {
+
+        for (Integer num : values) {
+
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void hw2(List<Integer> values) {
+
+        Set<Integer> set = new TreeSet<>(values);
+
+        for (Integer num : set) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void hw3(String str) {
+
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(str.split(" ")));
+        for (String st : set) {
+            System.out.print(st + " ");
+        }
+        System.out.println();
+    }
+
+    public static void hw4(String str) {
+
+        List<String> stringList = Arrays.asList(str.split(" "));
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : stringList) {
+
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> m : map.entrySet()) {
+
+            if (m.getValue() > 1) {
+                System.out.print(m + " ");
+            }
+        }
+
+
+    }
+
+
 }
